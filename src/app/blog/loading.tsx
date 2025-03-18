56eeb1383893f9ec0dc40bcedf90a57c9cc94981
betawars@oregonstate.edu <betawars@oregonstate.edu>
2025-03-18 15:14:31 -0700

    24 - tut

---
+++ src/app/blog/loading.tsx
@@ -0,0 +1,6 @@
+// This file is an example on how the loading page in created in case any of the children pages are taking some time to load
+// THis is a good way to make your website look more interative and responsive to every click.
+
+export default function LoadingScreen(){
+    return<h1>Loading...</h1>
+}
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,15 @@
-export default function Blog(){
-    return<h1>Blog parent</h1>
+//This is a part of an example where a deliberate delay is created in order to show the functionality of the loading.tsx
+// THis can also be used to learn how a deliberate delay is created in case of rendering a page.
+
+
+export default async function Blog(){
+    await new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve("check")
+        },2000)
+    })
+    
+    return (
+        <h1>Blog Parent</h1>
+    )
 }
