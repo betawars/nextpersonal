7e9e1816bc5a99d911e7f0cde87516fc9b081549
betawars@oregonstate.edu <betawars@oregonstate.edu>
2025-03-19 15:39:24 -0700

    26 - tut

--- src/app/product/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/product/[productId]/reviews/[reviewId]/error.tsx
@@ -1,9 +1,32 @@
 "use client"
 
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
 // This is an example of the error file that will be displayed whenever there is an error encountered in the adjacent and the children page
 // error.jsx should always use use client directive
 
-export default function ErrorBoundary({error}:{error:Error}
+
+//Exp2
+//In case if you want to set up the retry button to setp the page to retry after failing to load
+// use the following code
+export default function ErrorBoundary({error, reset,}:{
+    error:Error
+    reset:()=>void;
+}
 ){
-    return <div>{error.message}</div>
+    //This chunk of code is used to refresh the code which is from the serverside
+    //as the reset prop can only refresh the client side pages
+    const router = useRouter()
+    const reload = () =>{
+        startTransition(()=>{
+            router.refresh();
+            reset();
+        })
+    }
+    return (<div>
+
+        <p>{error.message}</p>
+        <button onClick={()=>reload()}>Try again</button>
+        </div>)
 }
